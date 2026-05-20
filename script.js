8e650b3aa9a693a199ff1d29c7c8d6508a1d4c13
coder123454321 <115930973+coder123454321@users.noreply.github.com>
2026-05-20 12:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function isSameType(value1, value2) {
 	if(Number.isNaN(value1) && Number.isNaN(value2)){
 		return true;
 	}
+	 if (Number.isNaN(value1) || Number.isNaN(value2)) {
+    return false;
+  }
 	if(typeof value1===typeof value2){
 		return true;
 	}
